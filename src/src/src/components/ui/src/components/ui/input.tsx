557b077848f4242f30f3a83e41161308da6eef08
VIVEX <andrexvive@gmail.com>
2025-08-30 20:13:29 +0200

    Delete src/src directory

--- src/src/src/components/ui/src/components/ui/input.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={`w-full rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-black/20 ${props.className||''}`} />
-}
--- src/src/src/components/ui/src/components/ui/src/components/ui/src/components/ui/card.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-export function Card({ className = '', ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div {...props} className={`border border-gray-200 bg-white ${className}`} />
-}
-export function CardContent({ className = '', ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div {...props} className={`p-4 ${className}`} />
-}
